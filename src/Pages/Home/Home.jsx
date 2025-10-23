8dd994293a23173c51af1f5dd183d0967d8a004d
tumpa <tumpadas3494@gmail.com>
2025-10-23 12:10:51 +0600

    testimonials.json added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import HeroSection from "./Sections/HeroSection";
 import PopularSkills from "./Sections/PopularSkills";
-import SkillDetails from "./Sections/SkillDetails";
+import TopCourseMarquee from "./Sections/TopCourseMarquee";
+import HowitWorks from "./Sections/HowitWorks";
+import Testimonials from "./Sections/Testimonials";
+// import SkillDetails from "./Sections/SkillDetails";
 
 
 const Home = () => {
   return (
     <div className="home-page">
       <HeroSection />
+
       <div className="max-w-10/12 mx-auto mt-20 px-8">
 <h2 className="text-center text-5xl text-[#2f2f2f] mb-4 font-bold">
   Popular Skills
 </h2>
 <p className="text-center mb-8 text-md w-2/4 mx-auto ">Connect with locals to exchange today’s most popular skills in a trusted space. Turn your talents into opportunities with an easy-to-use local skills marketplace.</p>
       <PopularSkills/>
-      {/* <SkillDetails/> */}
+      
+      </div>
+
+      <div className=" mt-20">
+
+        <TopCourseMarquee/>
+      </div>
+      <div>
+        <Testimonials/>
+      </div>
+
+      <div className=" mt- bg-gradient-to-r from-indigo-50 via-white to-indigo-50" >
+        <HowitWorks/>
       </div>
       
     </div>
